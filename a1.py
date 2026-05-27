29d20bad88314fc1efdcf1a82fa09e5964b8d07e
PrachiJunwaleCodingal <prachi.junwale@gmail.com>
2026-05-27 13:39:19 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,8 @@
+def fun1(n):
+
+    return n*(n+1)/2
+
+print(fun1(4))
+
+
+# number of iteration will be only one for input 4
---
+++ a2.py
@@ -0,0 +1,10 @@
+def fun2(n):
+    sum=0
+    for i in range(1,n+1):
+        sum+=i
+    return sum
+
+
+print(fun2(4))
+
+# number of iteration will 4 ,for input 4
